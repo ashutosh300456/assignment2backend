bf1e3ad8ecaba242836f8452a94f47a8f71e3b1e
ashutosh300456 <ashutoshtiwariana@gmail.com>
2024-08-28 23:30:18 +0530

    changes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const cors = require('cors');
 const app = express();
 const PORT=process.env.PORT || 5000
 app.use(cors({
-  origin: '*', // No trailing slashes
+  origin: 'https://assignment2backend.onrender.com/signup', // No trailing slashes
   methods: 'GET,POST,PUT,DELETE', // Allowed methods
   credentials: true // Allow cookies if necessary
 }));
