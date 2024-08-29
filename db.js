6144290182c37c779e159b60840973ae38e153f5
ashutosh300456 <ashutoshtiwariana@gmail.com>
2024-08-29 10:00:10 +0530

    changes

--- db.js
+++ db.js
@@ -2,36 +2,13 @@ const mongoose = require("mongoose");
 const mongooseURI = "mongodb://localhost:27017/school";    
 
 const connectToMongo = () => {
-   mongoose.connect(mongooseURI);
-  console.log("final done");
+  mongoose.connect(mongooseURI)
+    .then(() => {
+      console.log("MongoDB connected successfully");
+    })
+    .catch((error) => {
+      console.error("Error connecting to MongoDB:", error);
+    });
 };
 
 module.exports = connectToMongo;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
